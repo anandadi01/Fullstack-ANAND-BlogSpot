fa5b082de819f7e633cf7cda18a082db53ab738c
Aditya Anand <20bec004@iiitdmj.ac.in>
2023-12-17 01:25:15 +0530

    fix: vercel config

--- fullstack_blogging/wsgi.py
+++ fullstack_blogging/wsgi.py
@@ -11,7 +11,17 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fullstack_blogging.settings')
 
 app = get_wsgi_application()
-app = app
+# app = app
+
+def handler(event, context):
+    response = app(event['body'], event['headers'])
+    return{
+        'statusCode': response.status_code,  
+        'body': response.content,  
+        'headers': dict(response.headers)
+    }
